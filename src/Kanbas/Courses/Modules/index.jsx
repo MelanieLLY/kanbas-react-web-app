096849abe24aa01b5177f2e37084e4b78330e5aa
MelanieLLY <105343088+MelanieLLY@users.noreply.github.com>
2024-10-03 17:55:09 -0700

    update

--- src/Kanbas/Courses/Modules/index.jsx
+++ src/Kanbas/Courses/Modules/index.jsx
@@ -1,65 +1,45 @@
+import ModulesControls from "./ModulesControls";
 export default function Modules() {
   return (
-    <div>
-      <br />
-      <br />
-      <button>Collapes</button>
-      <button>View all</button>
-      <select>
-        <option value="VAL1">Publish Some</option>
-        <option value="VAL2" selected>
-          Publish All
-        </option>
-        <option value="VAL3">Publish None</option>
-      </select>
-      <button>+ Module</button>
-      <br />
-      <br />
-      <ul id="wd-modules">
-        <li className="wd-module">
-          <div className="wd-title">Week 1</div>
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">LEARNING OBJECTIVES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Introduction to the course</li>
-                <li className="wd-content-item">
-                  Learn what is Web Development
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
+<div>
+  <ModulesControls /><br /><br /><br /><br />
+  
+  <ul id="wd-modules" className="list-group rounded-0">
+    
+    {/* Wek 1 */}
+    <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+      <div className="wd-title p-3 ps-2 bg-secondary"> Week 1 </div>
+      <ul className="wd-lessons list-group rounded-0">
+        <li className="wd-lesson list-group-item p-3 ps-1">LEARNING OBJECTIVES</li>
+        <li className="wd-lesson list-group-item p-3 ps-1">Introduction to the course</li>
+        <li className="wd-lesson list-group-item p-3 ps-1">Learn what is Web Development</li>
 
-        <li className="wd-module">
-          <div className="wd-title">Week 2</div>
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">LEARNING OBJECTIVES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Introduction to HTML</li>
-                <li className="wd-content-item">
-                  Basic HTML tags and structure
-                </li>
-                <li className="wd-content-item">Creating our first website</li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-
-        <li className="wd-module">
-          <div className="wd-title">Week 3</div>
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">LEARNING OBJECTIVES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Introduction to CSS</li>
-                <li className="wd-content-item">Understanding CSS selectors</li>
-              </ul>
-            </li>
-          </ul>
-        </li>
       </ul>
-    </div>
+    </li>
+    
+    {/* Week 2 */}
+    <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+      <div className="wd-title p-3 ps-2 bg-secondary"> Week 2 </div>
+      <ul className="wd-lessons list-group rounded-0">
+        <li className="wd-lesson list-group-item p-3 ps-1">LEARNING OBJECTIVES</li>
+        <li className="wd-lesson list-group-item p-3 ps-1">Introduction to HTML</li>
+        <li className="wd-lesson list-group-item p-3 ps-1">Basic HTML tags and structure</li>
+        <li className="wd-lesson list-group-item p-3 ps-1">Creating our first website</li>
+      </ul>
+    </li>
+    
+    {/* Week 3 */}
+    <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+      <div className="wd-title p-3 ps-2 bg-secondary"> Week 3 </div>
+      <ul className="wd-lessons list-group rounded-0">
+        <li className="wd-lesson list-group-item p-3 ps-1">LEARNING OBJECTIVES</li>
+        <li className="wd-lesson list-group-item p-3 ps-1">Introduction to CSS</li>
+        <li className="wd-lesson list-group-item p-3 ps-1">Understanding CSS selectors</li>
+      </ul>
+    </li>
+
+  </ul>
+</div>
+
   );
 }
