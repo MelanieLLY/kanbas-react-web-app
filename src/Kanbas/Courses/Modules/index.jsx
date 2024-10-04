671d50a0a2b89f9534367e8908dfc6ff26202dd5
MelanieLLY <105343088+MelanieLLY@users.noreply.github.com>
2024-10-03 19:03:09 -0700

    update

--- src/Kanbas/Courses/Modules/index.jsx
+++ src/Kanbas/Courses/Modules/index.jsx
@@ -1,4 +1,7 @@
 import ModulesControls from "./ModulesControls";
+import { BsGripVertical } from 'react-icons/bs';
+import LessonControlButtons from "./LessonControlButtons";
+import ModuleControlButtons from "./ModuleControlButtons";
 export default function Modules() {
   return (
 <div>
@@ -8,33 +11,40 @@ export default function Modules() {
     
     {/* Wek 1 */}
     <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-      <div className="wd-title p-3 ps-2 bg-secondary"> Week 1 </div>
+      <div className="wd-title p-3 ps-2 bg-secondary"> 
+      <BsGripVertical className="me-2 fs-3" />
+
+        Week 1         <ModuleControlButtons />
+
+        </div>
       <ul className="wd-lessons list-group rounded-0">
-        <li className="wd-lesson list-group-item p-3 ps-1">LEARNING OBJECTIVES</li>
-        <li className="wd-lesson list-group-item p-3 ps-1">Introduction to the course</li>
-        <li className="wd-lesson list-group-item p-3 ps-1">Learn what is Web Development</li>
+        <li className="wd-lesson list-group-item p-3 ps-1">          <BsGripVertical className="me-2 fs-3" />
+        LEARNING OBJECTIVES          <LessonControlButtons />
+        </li>
+        <li className="wd-lesson list-group-item p-3 ps-1"><BsGripVertical className="me-2 fs-3" />Introduction to the course<LessonControlButtons /></li>
+        <li className="wd-lesson list-group-item p-3 ps-1"><BsGripVertical className="me-2 fs-3" />Learn what is Web Development<LessonControlButtons /></li>
 
       </ul>
     </li>
     
     {/* Week 2 */}
     <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-      <div className="wd-title p-3 ps-2 bg-secondary"> Week 2 </div>
+      <div className="wd-title p-3 ps-2 bg-secondary"><BsGripVertical className="me-2 fs-3" /> Week 2<ModuleControlButtons /> </div>
       <ul className="wd-lessons list-group rounded-0">
-        <li className="wd-lesson list-group-item p-3 ps-1">LEARNING OBJECTIVES</li>
-        <li className="wd-lesson list-group-item p-3 ps-1">Introduction to HTML</li>
-        <li className="wd-lesson list-group-item p-3 ps-1">Basic HTML tags and structure</li>
-        <li className="wd-lesson list-group-item p-3 ps-1">Creating our first website</li>
+        <li className="wd-lesson list-group-item p-3 ps-1"><BsGripVertical className="me-2 fs-3" />LEARNING OBJECTIVES<LessonControlButtons /></li>
+        <li className="wd-lesson list-group-item p-3 ps-1"><BsGripVertical className="me-2 fs-3" />Introduction to HTML<LessonControlButtons /></li>
+        <li className="wd-lesson list-group-item p-3 ps-1"><BsGripVertical className="me-2 fs-3" />Basic HTML tags and structure<LessonControlButtons /></li>
+        <li className="wd-lesson list-group-item p-3 ps-1"><BsGripVertical className="me-2 fs-3" />Creating our first website<LessonControlButtons /></li>
       </ul>
     </li>
     
     {/* Week 3 */}
     <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-      <div className="wd-title p-3 ps-2 bg-secondary"> Week 3 </div>
+      <div className="wd-title p-3 ps-2 bg-secondary"> <BsGripVertical className="me-2 fs-3" />Week 3 <ModuleControlButtons /></div>
       <ul className="wd-lessons list-group rounded-0">
-        <li className="wd-lesson list-group-item p-3 ps-1">LEARNING OBJECTIVES</li>
-        <li className="wd-lesson list-group-item p-3 ps-1">Introduction to CSS</li>
-        <li className="wd-lesson list-group-item p-3 ps-1">Understanding CSS selectors</li>
+        <li className="wd-lesson list-group-item p-3 ps-1"><BsGripVertical className="me-2 fs-3" />LEARNING OBJECTIVES<LessonControlButtons /></li>
+        <li className="wd-lesson list-group-item p-3 ps-1"><BsGripVertical className="me-2 fs-3" />Introduction to CSS<LessonControlButtons /></li>
+        <li className="wd-lesson list-group-item p-3 ps-1"><BsGripVertical className="me-2 fs-3" />Understanding CSS selectors<LessonControlButtons /></li>
       </ul>
     </li>
 
