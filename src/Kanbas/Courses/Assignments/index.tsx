b7017ca04ef1984ec0a16baf7980720b33ed3c7b
MelanieLLY <105343088+MelanieLLY@users.noreply.github.com>
2024-10-05 11:51:35 -0700

    fix bug

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -1,4 +1,8 @@
-import { FaPlusCircle, FaSearch, FaBook } from "react-icons/fa";
+import { FaPlusCircle, FaSearch, FaBook, FaPlus } from "react-icons/fa";
+import { GoPlus } from "react-icons/go";
+import { MdArrowDropDown } from "react-icons/md";
+
+
 import { BsThreeDotsVertical, BsGripVertical } from "react-icons/bs";
 import AssLessonControlButtons from "./AssLessonControlButtons";
 
@@ -20,32 +24,32 @@ export default function Assignments() {
         </div>
         {/* search right      */}
         <div>
-          <button className="btn btn-secondary me-2">
-            <FaPlusCircle className="me-1" /> Group
+          <button className="btn btn-secondary m-1">
+          <GoPlus className="fs-4" /> Group
           </button>
 
-          <button className="btn btn-danger">
-            <FaPlusCircle className="me-1" /> Assignment
+          <button className="btn btn-danger m-1">
+            <GoPlus className="fs-4" /> Assignment
           </button>
         </div>
       </div>
+
       {/* assignment title */}
 
       <div className="d-flex justify-content-between align-items-center p-2 bg-light border border-gray">
         <div className="d-flex align-items-center">
-          <BsGripVertical className="me-2 fs-4" />
-          <span className="me-2">▼</span>
+          <BsGripVertical className="mb-1 me-2 fs-4" />
+          <MdArrowDropDown className="fs-2" />
           <h3 className="m-0">ASSIGNMENTS</h3>
         </div>
         <div className="d-flex align-items-center">
           <div
             className="border border-secondary rounded-pill px-3 py-1 me-2"
-            style={{ display: "inline-block" }}
           >
             40% of Total
           </div>
 
-          <FaPlusCircle className="me-3 fs-5" />
+          <GoPlus className="me-3 fs-4" />
           <BsThreeDotsVertical className="fs-5" />
         </div>
       </div>
@@ -65,22 +69,24 @@ export default function Assignments() {
 
               <div className="assignment-icon d-flex align-items-center me-3">
                 <BsGripVertical className="me-2 fs-4" />
-                <FaBook className="fs-4 text-muted" />
+                <FaBook className="fs-4 text-success" />
               </div>
 
               <div className="assignment-info">
                 <a
-                  className="wd-assignment-link fw-bold text-decoration-none"
+                  className="wd-assignment-link fw-bold text-decoration-none text-dark"
                   href="#/Kanbas/Courses/CS1234/Assignments/123"
+                  // style={{ color: "black", fontWeight: "bold" }}
                 >
                   A1 - ENV + HTML
                 </a>
                 <p className="wd-assignment-overview text-muted">
-                  Multiple Modules | <strong>Not available until:</strong>{" "}
-                  <span>2024-09-12 09:00 AM</span> |<strong>Due:</strong>{" "}
-                  <span>2024-09-12 11:59 PM</span> | Total 100
-                </p>
+                <span className="text-danger">Multiple Modules</span> | 
+                <strong> Not available until:</strong> <span>2024-09-12 09:00 AM</span> | 
+                <strong> Due:</strong> <span>2024-09-12 11:59 PM</span> | 100 pts
+              </p>
               </div>
+
               <div className="ms-auto">
                 <AssLessonControlButtons />
               </div>
@@ -96,19 +102,19 @@ export default function Assignments() {
               ></div>
               <div className="assignment-icon d-flex align-items-center me-3">
                 <BsGripVertical className="me-2 fs-4" />
-                <FaBook className="fs-4 text-muted" />
+                <FaBook className="fs-4 text-success" />
               </div>
               <div className="assignment-info">
                 <a
-                  className="wd-assignment-link fw-bold text-decoration-none"
+                  className="wd-assignment-link fw-bold text-decoration-none text-dark"
                   href="#/Kanbas/Courses/CS1234/Assignments/124"
                 >
                   A2 - CSS Basics
                 </a>
                 <p className="wd-assignment-overview text-muted">
-                  Single Module | <strong>Not available until:</strong>{" "}
-                  <span>2024-09-19 09:00 AM</span> |<strong>Due:</strong>{" "}
-                  <span>2024-09-19 11:59 PM</span> | Total 100
+                <span className="text-danger">Single Modules</span> | <strong>Not available until:</strong>{" "}
+                  <span>2024-09-19 09:00 AM</span> | <strong>Due:</strong>{" "}
+                  <span>2024-09-19 11:59 PM</span> | 100 pts
                 </p>
               </div>
               <div className="ms-auto">
@@ -126,19 +132,19 @@ export default function Assignments() {
               ></div>
               <div className="assignment-icon d-flex align-items-center me-3">
                 <BsGripVertical className="me-2 fs-4" />
-                <FaBook className="fs-4 text-muted" />
+                <FaBook className="fs-4 text-success" />
               </div>
               <div className="assignment-info">
                 <a
-                  className="wd-assignment-link fw-bold text-decoration-none"
+                  className="wd-assignment-link fw-bold text-decoration-none text-dark"
                   href="#/Kanbas/Courses/CS1234/Assignments/125"
                 >
                   A3 - Responsive Design
                 </a>
                 <p className="wd-assignment-overview text-muted">
-                  Multiple Modules | <strong>Not available until:</strong>{" "}
-                  <span>2024-09-26 09:00 AM</span> |<strong>Due:</strong>{" "}
-                  <span>2024-09-26 11:59 PM</span> | Total 100
+                <span className="text-danger">Single Modules</span> | <strong>Not available until:</strong>{" "}
+                  <span>2024-09-26 09:00 AM</span> | <strong>Due:</strong>{" "}
+                  <span>2024-09-26 11:59 PM</span> | 100 pts
                 </p>
               </div>
               <div className="ms-auto">
