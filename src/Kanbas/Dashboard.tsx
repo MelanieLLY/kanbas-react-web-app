a4d3d62166fd19739ad035c0afb1f1b017beb39d
MelanieLLY <105343088+MelanieLLY@users.noreply.github.com>
2024-11-07 17:20:42 -0800

    update

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import * as db from "./Database";
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function Dashboard({ courses, course, setCourse, addNewCourse,
   deleteCourse, updateCourse }: {
@@ -45,7 +46,8 @@ export default function Dashboard({ courses, course, setCourse, addNewCourse,
   //   setCourses(courses.filter((course) => course._id !== courseId));
   // };
 
-  
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const { enrollments } = db;
 
   return (
     <div id="wd-dashboard">
@@ -86,7 +88,14 @@ export default function Dashboard({ courses, course, setCourse, addNewCourse,
       <hr />
       <div id="wd-dashboard-courses" className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
-          {courses.map((course) => (
+        {courses
+    .filter((course) =>
+      enrollments.some(
+        (enrollment) =>
+          enrollment.user === currentUser._id &&
+          enrollment.course === course._id
+         ))
+.map((course) => (
             <div className="wd-dashboard-course col" style={{ width: "300px" }}>
               <div className="card rounded-3 overflow-hidden h-100 d-flex flex-column">
                 <Link
